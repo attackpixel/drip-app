3c57ec1d74cb86ad3dd5efabfb57dd9dad64db32
pixelattack2686 <jackredpath10@gmail.com>
2026-02-15 09:44:17 +0000

    Add GitHub Actions workflow for Android EAS build

--- drip/apps/mobile/metro.config.js
+++ drip/apps/mobile/metro.config.js
@@ -2,17 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
 const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
-
-// Watch all files in the monorepo
-config.watchFolders = [...new Set([...(config.watchFolders ?? []), monorepoRoot])];
-
-// Let Metro resolve packages from the monorepo root
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-];
 
 module.exports = config;
